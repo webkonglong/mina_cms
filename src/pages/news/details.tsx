f9a8bbd3429c31f0daa483460f627cdaf2fde744
webkonglong <anmingzhe@me.com>
2021-04-24 16:52:47 +0800

    [feat]

--- src/pages/news/details.tsx
+++ src/pages/news/details.tsx
@@ -1,11 +1,13 @@
 import originData from './data.tsx';
 import styles from './news.less';
 import { useLocation } from 'umi';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { StoreContext } from '@/context/languageContext';
 
 export default () => {
   const location = useLocation();
   const [data, setData] = useState<string>('');
+  const { state } = useContext(StoreContext);
 
   useEffect(() => {
     if (location?.query?.id) {
@@ -16,12 +18,14 @@ export default () => {
 
   return (
     <div className={styles.details}>
-      <div className={styles.title}>{data.zh_name}</div>
+      <div className={styles.title}>
+        {state.language === 'zh' ? data.zh_name : data.en_name}
+      </div>
       <div className={styles.time}>
         <span style={{ marginRight: '20px' }}>{data.time}</span>
         <span>{data?.source}</span>
       </div>
-      {data?.zh_body}
+      {state.language === 'zh' ? data.zh_body : data.en_body}
     </div>
   );
 };
---
+++ src/pages/news/i18n.ts
@@ -0,0 +1,18 @@
+export default {
+  zh: {
+    all: '全部',
+    token: '代币',
+    technology: '技术',
+    forums: '社区',
+    case: '案例',
+    banner: 'Mina 是一个快速发展、有着广阔应用前景的项目。',
+  },
+  en: {
+    all: 'All',
+    token: 'Token',
+    technology: 'Technology',
+    forums: 'Forums',
+    case: 'Cases',
+    banner: 'Mina is a project with rapid development and wide applications.',
+  },
+};
