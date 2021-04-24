e7c82dd64551fd2b2a04606a95b8418d9a82b98b
webkonglong <anmingzhe@me.com>
2021-04-24 18:51:26 +0800

    [feat]

--- src/component/videoLoading.tsx
+++ src/component/videoLoading.tsx
@@ -1,8 +1,17 @@
 import Icon from './icon';
+import { StoreContext } from '@/context/languageContext';
+import { useContext } from 'react';
 export default () => {
+  const { state } = useContext(StoreContext);
+
   return (
     <div className="c-video-loading">
-      <Icon className="c-video-loading-icon" type="iconjiazaizhong" />
+      <Icon className="c-video-loading-icon" type="iconjiazaizhong1" />
+      <p className="c-video-loading-newwork">
+        {state.language === 'zh'
+          ? '视频需要vpn网络,请确认已打开vpn网络'
+          : 'The video needs a VPN network. Please confirm that the VPN network is turned on'}
+      </p>
     </div>
   );
 };
